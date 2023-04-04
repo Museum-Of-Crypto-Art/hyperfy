7ae20c2df6b821433c110f7c7a473dfdf86ab0bb
kleintonno <michael.jathe@protonmail.com>
2023-04-04 15:53:47 +0200

    live build of the app

--- apps/moca-rooms-v1/index.js
+++ apps/moca-rooms-v1/index.js
@@ -1,151 +1,92 @@
-'use strict'
+import React, { useEffect } from 'react'
+import { useWorld, useFields, useSyncState } from 'hyperfy'
 
-Object.defineProperty(exports, '__esModule', { value: true })
+export default function App() {
+  const world = useWorld()
+  const fields = useFields()
+  const [state, dispatch] = useSyncState(s => s)
 
-const React = require('react')
-const hyperfy = require('hyperfy')
-
-function _interopDefaultLegacy(e) {
-  return e && typeof e === 'object' && 'default' in e ? e : { default: e }
-}
-
-var React__default = /*#__PURE__*/ _interopDefaultLegacy(React)
-
-const sendInfo = (msg, world) => {
-  editing && world.chat(msg, true)
-  console.log(msg)
-}
-
-function app() {
-  const world = hyperfy.useWorld()
-  const fields = hyperfy.useFields()
-  const editing = hyperfy.useEditing()
-  const loadRoomCurated = fields.curated
   const roomId = fields.roomId
+  const curated = fields.curated
+  const scale = fields.scale
+  const collision = fields.collision
 
-  const [roomURL, setRoomURL] = React.useState(null)
+  const src = curated ? state.curatedSrc : state.emptySrc
+  const msg = state.msg
 
-  const [roomData, dispatch] = hyperfy.useSyncState(state => state.data)
-
-  const sendInfo = msg => {
-    editing && world.chat(msg, true)
-    console.log(msg)
-  }
-
-  React.useEffect(() => {
-    if (!world.isClient) return
-    if (roomId === '') return
-    async function getRoom() {
+  useEffect(() => {
+    if (!world.isServer) return
+    if (!roomId) return
+    async function load() {
+      dispatch('setMsg', 'Loading...')
       try {
         const data = await world.http({
           method: 'GET',
-          url: 'https://api.museumofcryptoart.com/oracle/rooms/' + roomId,
+          url: `https://api.museumofcryptoart.com/oracle/rooms/${roomId}`,
           headers: {
             'Content-Type': 'application/json',
           },
         })
         if (data) {
-          sendInfo('ROOM #' + roomId + ' loaded', world)
-          dispatch('update', data)
-        }
-      } catch (e) {
-        console.error(e)
-      }
-    }
-    getRoom()
-  }, [])
-
-  hyperfy.useSignal('Load ROOM', () => {
-    if (!world.isClient) return
-    if (roomId === '') return
-    async function getRoom() {
-      try {
-        const data = await world.http({
-          method: 'GET',
-          url: 'https://api.museumofcryptoart.com/oracle/rooms/' + roomId,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        })
-
-        if (data) {
-          sendInfo('ROOM #' + roomId + ' loaded', world)
-          dispatch('update', data)
+          const emptySrc = data.room.model
+          const curatedSrc = data.room.updatedBy
+            ? data.room.model_curated
+            : emptySrc
+          dispatch('setSrc', emptySrc, curatedSrc)
+          dispatch('setMsg', null)
         } else {
-          sendInfo('ROOM #' + roomId + ' does not exist.', world)
+          dispatch('setMsg', 'Invalid ROOM ID.')
         }
-      } catch (e) {
-        console.error(e)
+      } catch (err) {
+        console.error(err)
+        dispatch('setMsg', 'Could not load ROOM.')
       }
     }
-    getRoom()
-  })
+    load()
+  }, [roomId])
 
-  React.useEffect(() => {
-    if (!roomData.room) return
-    try {
-      if (loadRoomCurated) {
-        if (roomData.room.updatedBy) {
-          setRoomURL(roomData.room.model_curated)
-          sendInfo(
-            'This ROOM has been curated by ' + roomData.room.updatedBy,
-            world
-          )
-        } else {
-          setRoomURL(roomData.room.model)
-          sendInfo(
-            'No curation has been saved for room ' +
-              roomId +
-              ' yet. The empty ROOM is loaded instead. If ROOM ' +
-              roomId +
-              ' is yours, go to https://rooms.museumofcryptoart.com/rooms/' +
-              roomId +
-              ' and save a curation for this ROOM.',
-            world
-          )
-        }
-      } else {
-        setRoomURL(roomData.room.model)
-      }
-    } catch (e) {
-      console.error(e)
-    }
-  }, [roomData, loadRoomCurated])
-
-  return React__default['default'].createElement(
-    'app',
-    null,
-    /*#__PURE__*/ React__default['default'].createElement(
-      'rigidbody',
-      {
-        type: 'kinematic',
-      },
-      roomURL !== null &&
-        React__default['default'].createElement('model', {
-          src: roomURL,
-          scale: fields.scale,
-          collision: fields.collision ? 'trimesh' : undefined,
-        }),
-      roomURL === null &&
-        React__default['default'].createElement('model', {
-          src: 'block-model.glb',
-          collision: fields.collision ? 'trimesh' : undefined,
-        })
-    )
+  return (
+    <app>
+      <rigidbody type="kinematic">
+        {src && (
+          <model
+            src={src}
+            scale={scale}
+            collision={collision ? 'trimesh' : undefined}
+          />
+        )}
+        {!src && (
+          <model
+            src="block-model.glb"
+            collision={collision ? 'trimesh' : undefined}
+          />
+        )}
+        {msg && (
+          <billboard axis="y" position={[0, 2, 0]}>
+            <text color="white" value={msg} fontSize={0.2} />
+          </billboard>
+        )}
+      </rigidbody>
+    </app>
   )
 }
-const initialState = { data: [] }
-function getStore() {
-  let state =
-    arguments.length > 0 && arguments[0] !== undefined
-      ? arguments[0]
-      : initialState
 
+const initialState = {
+  msg: null,
+  emptySrc: null,
+  curatedSrc: null,
+}
+
+export function getStore(state = initialState) {
   return {
     state,
     actions: {
-      update(state, data) {
-        state.data = data
+      setMsg(state, msg) {
+        state.msg = msg
+      },
+      setSrc(state, emptySrc, curatedSrc) {
+        state.emptySrc = emptySrc
+        state.curatedSrc = curatedSrc
       },
     },
     fields: [
@@ -160,6 +101,7 @@ function getStore() {
         label: 'ROOM ID',
         type: 'text',
         placeholder: 'token id of your ROOM',
+        instant: false,
       },
       {
         key: 'scale',
@@ -199,14 +141,6 @@ function getStore() {
         ],
         initial: true,
       },
-      {
-        key: '__lockedScale',
-        type: 'vec3',
-        hidden: true,
-      },
     ],
   }
 }
-
-exports['default'] = app
-exports.getStore = getStore
